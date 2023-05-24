6512ca815e6e7c3672bd9b48230bdf842f70f3fc
beta <santibeta82@gmail.com>
2023-05-23 20:18:07 -0500

    fixed footer link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="w-full py-4 shadow-inner bg-white">
         <p class="text-center">Made with ❤️ by <a href="https://www.linkedin.com/in/santiago-betancur/"
-                class="underline">sbetav</a>
+                class="underline" target="_blank">sbetav</a>
         </p>
     </footer>
 </template>
